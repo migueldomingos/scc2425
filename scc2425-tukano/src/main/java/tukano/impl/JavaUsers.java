59ec7544198a33acbfc0106aade7dc7e7311cb1b
Miguel Domingos <mf.domingos@campus.fct.unl.pt>
2024-10-26 02:06:44 +0100

    swegaliza

--- scc2425-tukano/src/main/java/tukano/impl/JavaUsers.java
+++ scc2425-tukano/src/main/java/tukano/impl/JavaUsers.java
@@ -11,10 +11,15 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.azure.cosmos.*;
 import com.azure.cosmos.models.CosmosItemRequestOptions;
 import com.azure.cosmos.models.PartitionKey;
+import com.azure.cosmos.models.CosmosQueryRequestOptions;
+import com.azure.cosmos.util.CosmosPagedIterable;
 import tukano.api.Result;
 import tukano.api.User;
 import tukano.api.Users;
@@ -39,8 +44,8 @@ public class JavaUsers implements Users {
 	private JavaUsers() {
 
 		client = new CosmosClientBuilder()
-				.endpoint(System.getProperty("COLOCAR AQUI URL!!!!!!!!!!!!!!!!!!!!!!"))
-				.key(System.getProperty("COLOCAR AQUI KEY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
+				.endpoint(System.getProperty("COSMOSDB_URL"))
+				.key(System.getProperty("COSMOSDB_KEY"))
 				//.directMode()
 				.gatewayMode()
 				// replace by .directMode() for better performance
@@ -49,7 +54,7 @@ public class JavaUsers implements Users {
 				.contentResponseOnWriteEnabled(true)
 				.buildClient();
 
-		db = client.getDatabase(System.getProperty("COLOCAR AQUI NOME DB!!!!!!!!!!!!!!!!!!!!!"));
+		db = client.getDatabase(System.getProperty("COSMOSDB_DATABASE"));
 		container = db.getContainer(Users.NAME);
 	}
 	
@@ -100,7 +105,6 @@ public class JavaUsers implements Users {
 		if (other.getDisplayName() == null)
 			other.setDisplayName(oldUserResult.value().getDisplayName());
 
-
 		return tryCatch( () -> container.upsertItem(other).getItem());
 	}
 
@@ -127,13 +131,29 @@ public class JavaUsers implements Users {
 	public Result<List<User>> searchUsers(String pattern) {
 		Log.info( () -> format("searchUsers : patterns = %s\n", pattern));
 
-		var query = format("SELECT * FROM User u WHERE UPPER(u.userId) LIKE '%%%s%%'", pattern.toUpperCase());
-		var hits = DB.sql(query, User.class)
-				.stream()
-				.map(User::copyWithoutPassword)
-				.toList();
+		if (pattern == null || pattern.trim().isEmpty()) {
+			return error(BAD_REQUEST);
+		}
 
-		return ok(hits);
+		var query = format("SELECT * FROM User u WHERE CONTAINS(UPPER(u.userId), '%s')", pattern.toUpperCase());
+
+		List<User> users = new ArrayList<>();
+
+		try {
+			CosmosPagedIterable<User> results = container.queryItems(
+					query, new CosmosQueryRequestOptions(), User.class
+			);
+
+			// Process the results, copying the users without their password
+			users = results.stream()
+					.map(User::copyWithoutPassword)
+					.collect(Collectors.toList());
+
+		} catch (CosmosException e) {
+			return error(INTERNAL_ERROR);
+		}
+
+		return ok(users);
 	}
 
 	
